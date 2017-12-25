53b21eb38329b01b39577e580a19665247cbb22c
zengruoyu <januszry@gmail.com>
2017-12-25 21:16:37 +0800

    refactor eslint

--- src/scripts/audio.js
+++ src/scripts/audio.js
@@ -9,33 +9,32 @@ function toHex(i, padding = 2) {
     return hex
 }
 
-navigator.mediaDevices.getUserMedia({ audio: true, video: false})
-    .then(stream => {
-        const source = audioCtx.createMediaStreamSource(stream)
-        source.connect(analyser)
+export function analyse(stream) {
+    const source = audioCtx.createMediaStreamSource(stream)
+    source.connect(analyser)
 
-        analyser.minDecibels = -40
-        analyser.maxDecibels = -10
-        analyser.fftSize = 32
+    analyser.minDecibels = -40
+    analyser.maxDecibels = -10
+    analyser.fftSize = 32
 
-        // TODO: assumed sample rate to 44100
-        const frequencyDataArray = new Uint8Array(3)  // 44100 / 32 * 3 ~= 4k
-        const timeDomainDataArray = new Uint8Array(32)
+    // TODO: assumed sample rate to 44100
+    const frequencyDataArray = new Uint8Array(3)  // 44100 / 32 * 3 ~= 4k
+    const timeDomainDataArray = new Uint8Array(32)
 
-        function fetch() {
-            // Schedule next redraw
-            requestAnimationFrame(fetch)
-            // Get spectrum data
-            analyser.getByteFrequencyData(frequencyDataArray)
-            analyser.getByteTimeDomainData(timeDomainDataArray)
-            analyser.getByteTimeDomainData
-            if (frequencyDataArray.reduce((a, b) => a + b, 0) > 0) {
-                const r = toHex(frequencyDataArray[0])
-                const g = toHex(frequencyDataArray[1])
-                const b = toHex(frequencyDataArray[2])
-                const vol = Math.max(...timeDomainDataArray)
-                console.log(`#${r}${g}${b}`, vol)
-            }
+    function fetch() {
+        // Schedule next redraw
+        requestAnimationFrame(fetch)
+        // Get spectrum data
+        analyser.getByteFrequencyData(frequencyDataArray)
+        analyser.getByteTimeDomainData(timeDomainDataArray)
+        analyser.getByteTimeDomainData
+        if (frequencyDataArray.reduce((a, b) => a + b, 0) > 0) {
+            const r = toHex(frequencyDataArray[0])
+            const g = toHex(frequencyDataArray[1])
+            const b = toHex(frequencyDataArray[2])
+            const vol = Math.max(...timeDomainDataArray)
+            console.log(`#${r}${g}${b}`, vol)
         }
-        fetch()
-    })
+    }
+    fetch()
+}
