67d0b715bebe3396d801d722571dc63c2bb1adca
coppla <januszry@gmail.com>
2017-12-30 20:12:38 +0800

    refactor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,46 +9,37 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: false })
 
 const playButton = document.querySelector('.play-button')
 const redoButton = document.querySelector('.redo-button')
+const wrapper = document.getElementById('wrapper')
 const crepe = document.getElementById('crepe')
 const title = document.getElementById('title')
 const world = document.querySelector('#playground .world')
 const mirroredWorld = document.querySelector('#playground .mirrored-world')
 
 function prepare() {
-  visualize.init()
   store.status = 'PREPARING'
-  world.style.backgroundImage = `url(${imageURL})`
-  document.body.style.backgroundColor = '#EDECEC'
 
-  world.style.display = 'block'
-  mirroredWorld.style.display = 'none'
+  visualize.init()
+
+  world.style.backgroundImage = `url(${imageURL})`
+  wrapper.classList.remove('playing')
+
   const mirrorCanvas = mirroredWorld.querySelector('canvas')
   const mirrorContext = mirrorCanvas.getContext('2d')
   mirrorContext.clearRect(0, 0, mirrorCanvas.width, mirrorCanvas.height)
-  for (const e of [crepe, playButton, redoButton, title]) {
-    if (!e) {
-      continue
-    }
-    e.classList.remove('playing')
-  }
 }
 
 function play() {
-  if (store.status === 'PREPARING') {
-    store.status = 'PLAYING'
-    visualize.closeContainer()
-    visualize.zoomOut()
+  if (store.status === 'PLAYING') {
+    return
   }
-  for (const e of [crepe, playButton, redoButton, title]) {
-    if (!e) {
-      continue
-    }
-    e.classList.add('playing')
-  }
+  store.status = 'PLAYING'
+
+  visualize.closeContainer()
+  visualize.zoomOut()
+
   world.style.backgroundImage = 'none'
-  document.body.style.backgroundColor = '#404040'
-  world.style.display = 'none'
-  mirroredWorld.style.display = 'block'
+  wrapper.classList.add('playing')
+
   visualize.generate()
 }
 
