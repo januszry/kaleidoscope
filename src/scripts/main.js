fdf45322579e2e02135bf915404c0fbda9b7b4b0
coppla <januszry@gmail.com>
2017-12-30 11:28:00 +0800

    buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,16 @@ import * as visualize from './visualize'
 
 navigator.mediaDevices.getUserMedia({ audio: true, video: false })
   .then(audio.analyse)
+
+const playButton = document.querySelector('.play-button')
+const redoButton = document.querySelector('.redo-button')
+
+playButton.onclick = function() {
+  // TODO
+  console.log('Play!')
+}
+
+redoButton.onclick = function() {
+  visualize.clearAll()
+  visualize.init()
+}
--- src/scripts/visualize.js
+++ src/scripts/visualize.js
@@ -8,7 +8,15 @@ world.style.backgroundImage = `url(${imageURL})`
 // create an engine
 const engine = Matter.Engine.create()
 
-function init() {
+export function clearAll() {
+  for (const c of world.querySelectorAll('canvas')) {
+    c.remove()
+  }
+  Matter.World.clear(engine.world)
+  Matter.Engine.clear(engine)
+}
+
+export function init() {
   // create a renderer
   const render = Matter.Render.create({
     element: world,
