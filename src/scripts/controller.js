cb3a8778c82544d47bd93bf4453410da97016818
coppla <januszry@gmail.com>
2017-12-31 19:55:17 +0800

    touched head

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,6 +1,7 @@
 import * as nipplejs from 'nipplejs'
 
 import * as visualize from './visualize'
+import touchedHeadURL from '../assets/joystick-head-touched.png'
 
 export function initJoystick() {
   const joystickZone = document.getElementById('joystick-head')
@@ -12,6 +13,7 @@ export function initJoystick() {
   })
   const mask = document.getElementById('mask')
   const originHeight = 78
+  const headURL = joystickZone.src
   joystick
     .on('move', (evt, data) => {
       const tx = data.distance * Math.cos(data.angle.radian)
@@ -22,7 +24,11 @@ export function initJoystick() {
       mask.style.marginLeft = `${tx}px`
       mask.style.marginTop = `${-ty}px`
     })
+    .on('start', (evt, data) => {
+      joystickZone.src = touchedHeadURL
+    })
     .on('end', (evt, data) => {
+      joystickZone.src = headURL
       joystickZone.style.transform = `rotate(0deg)`
       joystickZone.style.height = `${originHeight}px`
       mask.style.marginLeft = 0
